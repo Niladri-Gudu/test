a43fdc555e9993484dbfc7fc5f41544665f27818
Niladri-Gudu <niladrigudu@gmail.com>
2024-03-31 12:38:35 +0530

    second

--- script.js
+++ script.js
@@ -13,16 +13,16 @@ gsap.to(".box", {
   }
 })
 
-// const lenis = new Lenis()
+const lenis = new Lenis()
 
-// lenis.on('scroll', (e) => {
-//   console.log(e)
-// })
+lenis.on('scroll', (e) => {
+  console.log(e)
+})
 
-// lenis.on('scroll', ScrollTrigger.update)
+lenis.on('scroll', ScrollTrigger.update)
 
-// gsap.ticker.add((time)=>{
-//   lenis.raf(time * 1000)
-// })
+gsap.ticker.add((time)=>{
+  lenis.raf(time * 1000)
+})
 
-// gsap.ticker.lagSmoothing(0)
+gsap.ticker.lagSmoothing(0)
