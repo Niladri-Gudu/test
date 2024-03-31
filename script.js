b1ac481143e819784a10ccc32e77803d5e1483a1
Niladri-Gudu <niladrigudu@gmail.com>
2024-03-31 12:36:12 +0530

    first

---
+++ script.js
@@ -0,0 +1,28 @@
+gsap.registerPlugin(ScrollTrigger)
+
+gsap.to(".box", {
+  // rotation: 360,
+  // x: 400,
+  duration: 2,
+  scrollTrigger: {
+    trigger: ".box",
+    start: "top 30%",
+    end: "center 20%",
+    markers: true,
+    toggleClass: "red",
+  }
+})
+
+// const lenis = new Lenis()
+
+// lenis.on('scroll', (e) => {
+//   console.log(e)
+// })
+
+// lenis.on('scroll', ScrollTrigger.update)
+
+// gsap.ticker.add((time)=>{
+//   lenis.raf(time * 1000)
+// })
+
+// gsap.ticker.lagSmoothing(0)
